b7376533603670857f302063370e83f1d144bb06
leonidas-timefly <46664914+leonidas-timefly@users.noreply.github.com>
2023-03-23 13:25:14 +0800

    Create moveFile.py

---
+++ moveFile.py
@@ -0,0 +1,38 @@
+import os
+import random
+import shutil
+import sys
+ 
+ 
+def moveFile(input1,save1,save2):
+    pathDir = os.listdir(input1)  # 取图片的原始路径
+    for dir in pathDir:
+        source_dir = input1 + dir
+        #train_dir = save1 + "/" + dir
+        test_dir = save2 + "/" + dir
+        if not os.path.exists(test_dir):
+            os.mkdir(test_dir)
+
+        random.seed(1)
+        pic_list = os.listdir(source_dir)
+        filenumber = len(pic_list)  # 原文件个数
+        rate = 0.2  # 抽取的验证集的比例，占总数据的多少
+        picknumber = int(filenumber * rate)  # 按照rate比例从文件夹中取一定数量图片
+        picknumber = 30
+        #print(pic_list)
+        print(source_dir)
+        print(picknumber)
+        sample = random.sample(pic_list, picknumber)  # 随机选取需要数量的样本图片
+        print(sample)
+
+        for flie_name in sample:
+            path_img=os.path.join(source_dir, flie_name)
+            shutil.copyfile(path_img, os.path.join(test_dir, flie_name))
+ 
+if __name__ == '__main__':
+    input_path1='train/'
+    save_train='train'
+    save_test='new'
+    if not os.path.exists(save_test):
+        os.mkdir(save_test)
+    moveFile(input_path1,save_train,save_test)
